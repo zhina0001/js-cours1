a1f4a0d80047b008980e13a618e9080ae50a1c4b
zhina0001 <150028400+zhina0001@users.noreply.github.com>
2023-11-08 17:16:24 +0600

    hw

--- homework.js
+++ homework.js
@@ -1,1 +1,233 @@
-console.log("my  home work");
+// 1 1-кун
+// let challenge = "30 days of Javascript"
+// console.log(challenge);
+// 2
+// let a = "30 days of Javascript"
+// console.log(a);
+// 3
+// let a = "30 days of Javascript"
+// console.log(a.length);
+// 4
+// let a = "30 days of Javascript"
+// console.log(a.toUpperCase())
+// 5
+// let a = "30 days of Javascript"
+// console.log(a.toLowerCase())
+// 6
+// let b = "30 days of Javascript";
+// let firstWord = b.substring(0, b.indexOf(' '));
+// console.log(firstWord);
+// 7
+// let d = "30 days of Javascript";
+// let result = d.replace("days of Javascript", "");
+// console.log(result);
+// 8
+// const str = "OMG poco.";
+// const soz = "Script";
+// if (str.includes(soz)) {
+//   console.log("Стрингде '" + soz + "' найдено.");
+// } else {
+//   console.log("Стрингде '" + soz + "' не найлено.");
+// }
+// 9
+// const a = "This is a sample string that we want to split";
+// const b = a.split(' '); 
+// console.log(b);
+// 10
+// const a = "30 Days of Javascript";
+// const b = a.split(' ');
+// console.log(b)
+// 11
+// const a = "Facebok, Google, Microsoft, Apple, IBM, Amazonacle, Amazon"
+// const b = a.split(', ')
+// console.log(b)
+// 12
+// let a = "30 Days Of Javascript";
+// a = a.replace("Javascript", "Python");
+// console.log(a)
+// 13
+// let a = "30 Days of Javascript";
+// let b = a.charAt(18);
+// console.log(b)
+// 14
+// let a = "30 Days Of JavaScript";
+// let b = a.charCodeAt(15);
+// console.log(b)
+// 15
+// let a = "30 Days Of JavaScript"
+// let b = a.indexOf("J");
+// console.log(b)
+// 16
+// let a = "30 Days Of JavaScript"
+// let b = a.lastIndexOf("S")
+// console.log(b)
+// 17
+// let a = "30 Days of javascript";
+// let b = a.indexOf("javascript");
+// console.log(b);
+// 18
+// let a = "30 Days of javascript";
+// let b = a.lastIndexOf("Days");
+// console.log(b);
+// 19
+// let a = "30 Days of javascript";
+// let b = a.search("javascript");
+// console.log(b);
+// 20
+// let a = '30 Days Of javascript';
+// let b = a.trim();  
+// console.log(b);
+// 21
+// let a = "30 Days of Javascript";
+// let b = a.startsWith("30 Days");
+// console.log(b)
+// 22
+// let a = "30 Days of Javascript";
+// let b = a.endsWith("30 Days");
+// console.log(b);
+// 23
+// let a = "30 Days of Javascript";
+// let b = a.match();
+// console.log(b);
+// 24
+// let a = "30 Days";
+// let b = "Javascript";
+// let v = a+b;
+// console.log(v);
+// 25
+// let a = "30 Days Javascript";
+// let b = a.repeat(2);
+// console.log(b)
+// const agree = confirm('18 old ers?')
+// agree
+//     ?console.log("kir")
+//     :console.log("kury")
+// const now = new Date()
+// console.log(now.getFullYear())
+
+// 1  2-кун
+// let firstName = "ANDEYGY";
+// let lastName = "Ayupov";
+// let country = "asyana";
+// let city = "Almaty";
+// let age = 13;
+// let isMarried = false;
+// let year = 2010;
+// console.log(typeof firstName); 
+// console.log(typeof lastName);  
+// console.log(typeof country);   
+// console.log(typeof city);      
+// console.log(typeof age);       
+// console.log(typeof isMarried); 
+// console.log(typeof year);   
+// console.log(typeof '10' === typeof 10); 
+// 2
+// console.log(typeof '10' === typeof 10);
+// 3
+// console.log(parseInt('9.8') === 10);
+// 4
+// let truthyStatement1 = 5 > 3;
+// let truthyStatement2 = "rok" !== "kk";
+// let truthyStatement3 = !!'koeeee';
+// 5
+// console.log(4 > 3);   
+// console.log(4 >= 3);  
+// console.log(4 < 3);    
+// console.log(4 <= 3);   
+// console.log(4 == 4);  
+// console.log(4 === 4); 
+// console.log(4 != 4);   
+// console.log(4 !== 4);  
+// console.log(4 != '4'); 
+// console.log(4 == '4');
+// console.log(4 === '4');
+// let python = 'python';
+// let jargon = 'jargon';
+// 6
+// console.log(python.length == jargon.length);
+// console.log(4 > 3 && 10 < 12);   
+// console.log(4 > 3 && 10 > 12);    
+// console.log(4 > 3 || 10 < 12);   
+// console.log(4 > 3 || 10 > 12);   
+// console.log(!(4 > 3));            
+// console.log(!(4 < 3));           
+// console.log(!(false));           
+// console.log(!(4 > 3 && 10 < 12)); 
+// console.log(!(4 > 3 && 10 > 12));
+// console.log(!(4 === '4'));
+// let dragon = 'dragon';
+// let python = 'python';
+// 7
+// let today = new Date();
+// let year = today.getFullYear();
+// let month = today.getMonth() + 1; 
+// let date = today.getDate();
+// let day = today.getDay();
+// let hours = today.getHours();
+// let minutes = today.getMinutes();
+// let secondsSince1970 = today.getTime() / 1000;
+// console.log('Year:', year);
+// console.log('Month:', month);
+// console.log('Date:', date);
+// console.log('Day:', day);
+// console.log('Hours:', hours);
+// console.log('Minutes:', minutes);
+// console.log('Seconds since January 1, 1970:', secondsSince1970);
+
+//1 3-кун   
+// let userAge = prompt("Enter your age:");
+
+// let age = parseInt(userAge);
+
+// if (isNaN(age)) {
+//   console.log("Please enter a valid age.");
+// } else {
+//   if (age >= 18) {
+//     console.log("You are old enough to drive.");
+//   } else {
+//     let years = 18 - age;
+//     console.log("You need to wait for " + years + " year(s) to turn 18 and be old enough to drive.");
+//   }
+// }
+
+
+//2
+// let userAge = prompt("How old are you?");
+
+// userAge = parseInt(userAge);
+
+// if (userAge >= 30) {
+//   console.log("You are old enough to drive.");
+// } else {
+//   let yearsToWait = 30 - userAge;
+//   if (yearsToWait === 1) {
+//     console.log("You need to wait 1 more year to turn 18.");
+//   } else {
+//     console.log("You need to wait " + yearsToWait + " more years to turn 30.");
+//   }
+// }
+
+//3
+// let a = 4;
+// let b = 3;
+// let result = compareNumbersWithTernaryOperator(a, b);
+
+// function compareNumbersWithTernaryOperator(a, b) {
+//   return a > b ? 'a больше, чем b' : 'a меньше b';
+// }
+
+// console.log(result);
+
+
+//4
+// let userInput = prompt("Enter a number:");
+// let number = parseInt(userInput);
+
+// let result = isEvenOrOdd(number);
+
+// function isEvenOrOdd(number) {
+//     const parity = ["even", "odd"];
+//     return `${number} is an ${parity[number % 2] || "invalid"} number`;
+// }
+
+// console.log(result);s  2-кун
